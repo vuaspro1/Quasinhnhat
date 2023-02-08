71a6bde0dda9c77da93fb10bd7fe83dbb61f85fb
PhamTan <85300640+vuaspro1@users.noreply.github.com>
2023-02-08 08:31:21 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chúc mừng sinh nhật Sữa nha!',
+    introDesc: `Chúc em qua tuổi mới càng ngày càng xinh đẹp và học giỏi nha 🎉🎁`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu em ngay từ cái nhìn đầu tiên 😍',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Em cũng vậy <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Cho anh cơ hội làm người yêu em nha <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Em đồng ý <33333333',
+    mess: 'Anh biết mà 🥰',
+    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/pham.vantan.0512' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
