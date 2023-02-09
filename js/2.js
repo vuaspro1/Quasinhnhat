ff6b21f1a3656929c07fa7500010df8d23cbdf60
PhamTan <85300640+vuaspro1@users.noreply.github.com>
2023-02-10 01:29:37 +0700

    update

---
+++ js/2.js
@@ -0,0 +1,16 @@
+function firstQuestion(){
+    var audio = new Audio('../sound/music.mp3');
+    $('.content').hide();
+    Swal.fire({
+        title: CONFIG.introTitle,
+        text: CONFIG.introDesc,
+        imageUrl: 'img/1.jpg',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: '#fff url("img/iput-bg.jpg")',
+        imageAlt: 'Custom image',
+        confirmButtonText: CONFIG.btnIntro
+      }).then(function(){
+        $('.content').show(200);
+      })
+}
--- js/main.js
+++ js/main.js
@@ -17,22 +17,7 @@ function init(){
     $('#no').text(CONFIG.btnNo)
 }
 
-function firstQuestion(){
-    var audio = new Audio('../sound/music.mp3');
-    $('.content').hide();
-    Swal.fire({
-        title: CONFIG.introTitle,
-        text: CONFIG.introDesc,
-        imageUrl: 'img/1.jpg',
-        imageWidth: 300,
-        imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
-        imageAlt: 'Custom image',
-        confirmButtonText: CONFIG.btnIntro
-      }).then(function(){
-        $('.content').show(200);
-      })
-}
+
 
  // switch button position
  function switchButton() {
