4c09c74f863e1dfc51670a6fae096aa645e5f559
PhamTan <85300640+vuaspro1@users.noreply.github.com>
2023-02-09 10:35:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     introDesc: `Chúc em qua tuổi mới càng ngày càng xinh đẹp và học giỏi nha 🎉🎁
     (Tiện thể, chúc em thi tốt nha! Fighting)`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu em ngay từ cái nhìn đầu tiên 😍',
+    title: 'Phải chăng anh đã thích em ngay từ cái nhìn đầu tiên 😍',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Em cũng vậy <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Cho anh cơ hội làm người yêu em nha <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Nếu em không phiền thì tối nay đi ăn với anh nha!',
+    btnReply: 'Gửi cho anh',
     reply: 'Em đồng ý <33333333',
     mess: 'Anh biết mà 🥰',
-    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 8h anh qua đón nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/pham.vantan.0512' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
